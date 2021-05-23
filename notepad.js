8e7e771c11e0fbbf4c8b5b65e96086cdb8316944
pratik <pratikgaikwad551@gmail.com>
2021-05-23 08:29:53 +0530

    first-commit

---
+++ notepad.js
@@ -0,0 +1,17 @@
+const checkbox=document.getElementById("check");
+const textareaContent=document.getElementById("text-area");
+textareaContent.value=localStorage.getItem('autosave_data');
+
+checkbox.addEventListener('change',()=>{
+    //console.log("Hellow");
+    
+    if(checkbox.checked==true){
+        setInterval(function(){
+            localStorage.setItem('autosave_data',textareaContent.value);
+        },1000);
+        //console.log("Hellow");
+        //console.log(localStorage.getItem(autosave_data));
+    }
+    
+    
+})
